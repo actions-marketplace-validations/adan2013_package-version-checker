a8be69cbe830e8baedc3421daed3c4c768ca3b7e
Daniel Alberski <danielalberski@gmail.com>
2022-03-11 13:19:30 +0100

    script wrapped in async function

--- index.js
+++ index.js
@@ -13,46 +13,50 @@ const getPackageJson = async (ref, octokit, packageJsonPath) => {
   return JSON.parse(Buffer.from(packageJSONData, 'base64').toString())
 }
 
-try {
-  const token = core.getInput('GITHUB_TOKEN', { required: true })
-  const packageJsonPath = core.getInput('PACKAGE_JSON_PATH', { required: false })
-  const autoFailureMode = core.getInput('AUTO_FAILURE_MODE', { required: false })
-  const baseSHA = core.getInput('PR_BASE_SHA', { required: true })
-  const currentSHA = context.sha
+const compare = async () => {
+  try {
+    const token = core.getInput('GITHUB_TOKEN', { required: true })
+    const packageJsonPath = core.getInput('PACKAGE_JSON_PATH', { required: false })
+    const autoFailureMode = core.getInput('AUTO_FAILURE_MODE', { required: false })
+    const baseSHA = core.getInput('PR_BASE_SHA', { required: true })
+    const currentSHA = context.sha
 
-  const octokit = new GitHub(token)
-  const currentPackageJson = await getPackageJson(currentSHA, octokit, packageJsonPath)
-  const basePackageJson = await getPackageJson(baseSHA, octokit, packageJsonPath)
+    const octokit = new GitHub(token)
+    const currentPackageJson = await getPackageJson(currentSHA, octokit, packageJsonPath)
+    const basePackageJson = await getPackageJson(baseSHA, octokit, packageJsonPath)
 
-  let isChanged = true
-  if(currentPackageJson && basePackageJson) {
-    if(currentPackageJson.version === basePackageJson.version) {
-      isChanged = false
+    let isChanged = true
+    if(currentPackageJson && basePackageJson) {
+      if(currentPackageJson.version === basePackageJson.version) {
+        isChanged = false
+      }
     }
+
+    const currentVersion = currentPackageJson ? currentPackageJson.version : ''
+    const baseVersion = basePackageJson ? basePackageJson.version : ''
+    const packageName = currentPackageJson ? currentPackageJson.name : ''
+
+    core.setOutput('isChanged', isChanged)
+    core.setOutput('versionFromPR', currentVersion)
+    core.setOutput('versionFromBase', baseVersion)
+    core.setOutput('packageName', packageName)
+
+    switch (autoFailureMode) {
+      case 'isDifferent':
+        if(isChanged) core.setFailed(`Version number of package "${packageName}" cannot be changed manually!`)
+        break
+      case 'isTheSame':
+        if(!isChanged) core.setFailed(`Version number of package "${packageName}" is the same related to the base of PR!`)
+        break
+      case 'off':
+        core.info(`Version from base: ${baseVersion} Version from PR: ${currentVersion}`)
+        break
+      default:
+        core.setFailed(`Unknown auto failure mode: ${autoFailureMode}`)
+    }
+  } catch (err) {
+    core.setFailed(`Action failed with error ${err}`)
   }
-
-  const currentVersion = currentPackageJson ? currentPackageJson.version : ''
-  const baseVersion = basePackageJson ? basePackageJson.version : ''
-  const packageName = currentPackageJson ? currentPackageJson.name : ''
-
-  core.setOutput('isChanged', isChanged)
-  core.setOutput('versionFromPR', currentVersion)
-  core.setOutput('versionFromBase', baseVersion)
-  core.setOutput('packageName', packageName)
-
-  switch (autoFailureMode) {
-    case 'isDifferent':
-      if(isChanged) core.setFailed(`Version number of package "${packageName}" cannot be changed manually!`)
-      break
-    case 'isTheSame':
-      if(!isChanged) core.setFailed(`Version number of package "${packageName}" is the same related to the base of PR!`)
-      break
-    case 'off':
-      core.info(`Version from base: ${baseVersion} Version from PR: ${currentVersion}`)
-      break
-    default:
-      core.setFailed(`Unknown auto failure mode: ${autoFailureMode}`)
-  }
-} catch (err) {
-  core.setFailed(`Action failed with error ${err}`)
 }
+
+compare()
